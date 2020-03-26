4e0f259db88444451b7d06ad741272983ed076c8
julien dewerpe <julien.dewerpe@orange.fr>
2020-03-26 17:34:33 +0100

    API request, diplay of the Heroes List with picture

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
-function main(){	
-	function login() {
-		var Twit = require('twit');
-		var endpoints = require('./node_modules/twit/lib/endpoints');
-		endpoints.API_HOST      = 'https://cors-anywhere.herokuapp.com/https://api.twitter.com/';
-		endpoints.REST_ROOT     = 'https://cors-anywhere.herokuapp.com/https://api.twitter.com/1.1/';
-		endpoints.PUB_STREAM    = 'https://cors-anywhere.herokuapp.com/https://stream.twitter.com/1.1/';
-		endpoints.USER_STREAM   = 'https://cors-anywhere.herokuapp.com/https://userstream.twitter.com/1.1/';
-		endpoints.SITE_STREAM   = 'https://cors-anywhere.herokuapp.com/https://sitestream.twitter.com/1.1/';
-		endpoints.MEDIA_UPLOAD  = 'https://cors-anywhere.herokuapp.com/https://upload.twitter.com/1.1/';
-		endpoints.OA_REQ        = 'https://cors-anywhere.herokuapp.com/https://api.twitter.com/oauth/request_token';
-		endpoints.OA_ACCESS     = 'https://cors-anywhere.herokuapp.com/https://api.twitter.com/oauth/access_token';
+function research(name){
+    //todo : use fetch 
+    const Http = new XMLHttpRequest();
+    const corsURL = "https://cors-anywhere.herokuapp.com/";
+    const url='https://superheroapi.com/api/2463499803965064/search/' + name; //with the access token of Antoine
+    var urlReq = corsURL + url
+    Http.open("GET", urlReq);
+    Http.send();
+    Http.onreadystatechange = (e) => {
+        if(Http.readyState === 4 && Http.status === 200) {
+           
+          console.log(JSON.parse(Http.responseText))
+          var response = JSON.parse(Http.responseText)
+          var listChampions = response.results
+          listChampions.forEach(champion => {
+                addChampion(champion)
+		  });
 
-		var T = new Twit({
-  		consumer_key:         "Qb0zx5AeHmf9jzXDJjMTRUNmH",
-  		consumer_secret:      "0YC9uH6HkWbDwOSYdO4MRD0I7rz3crclijr07mMZZKkQ0TVRGM",
-  		access_token:         "564269211-K4joqVZIdCaySd5lMfeFMGmVizFJVjVylU2f30kb",
-  		access_token_secret:  "3u9WyRsZhvSdeU0qeOYPfrCTGGIzx4IBbyGdacAG0TLHj",
-  		timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-		strictSSL:            true,     // optional - requires SSL certificates to be valid.
-		})
-		//
-		//  tweet 'uwu'
-		
-		T.post('statuses/update', { status: 'uwu' }, function(err, data, response) {
-			console.log(data)
-		})
-		/*
-		var config = {
-			consumer_key:         "Qb0zx5AeHmf9jzXDJjMTRUNmH"
-		  , consumer_secret:      "0YC9uH6HkWbDwOSYdO4MRD0I7rz3crclijr07mMZZKkQ0TVRGM"
-		  , app_only_auth:        true
-		}
-		*/
-	}
-	var button = document.getElementById("but_log");
-	button.addEventListener('click', login);
+        }
+    }
+    
+
 }
 
-window.addEventListener('DOMContentLoaded', main);
+function clearChampList(){
+	var list = document.querySelector('#champ-list');
+	list.innerHTML = '';
+}
+
+function addChampion(champion){
+    var list = document.querySelector('#champ-list');
+	var champElmt = document.createElement("li");
+	var selectButton = document.createElement("button");
+	var image = document.createElement("img");
+    image.src = champion.image.url;
+    image.width = "50"
+	selectButton.appendChild(document.createTextNode("Select"))
+    selectButton.className = "btn btn-primary float-right"
+
+	champElmt.className = "list-group-item"
+	champElmt.appendChild(image)
+	champElmt.appendChild(document.createTextNode(champion.name))
+	champElmt.appendChild(selectButton)
+    list.appendChild(champElmt)
+}
+
+var searchButton = document.getElementById("search");
+
+searchButton.addEventListener('click', function(){
+    var heroName = document.getElementById("name").value;
+    clearChampList()
+    research(heroName)
+});
