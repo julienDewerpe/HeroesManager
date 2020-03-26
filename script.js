2aefd67461d8f37a33c35ef407ab6686162df6a8
julien dewerpe <julien.dewerpe@orange.fr>
2020-03-26 14:49:52 +0100

    start from scratch

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-function myFunction() {
-	let username = document.getElementById("login");
-	let password = document.getElementById("pwd");
-	alert(username.value);
-	alert(password.value);
-	const client = new Twitter({
-  		subdomain: "api", // "api" is the default (change for other subdomains)
-  		version: "1.1", // version "1.1" is the default (change for other subdomains)
-  		consumer_key: "Qb0zx5AeHmf9jzXDJjMTRUNmH", // from Twitter.
-  		consumer_secret: "0YC9uH6HkWbDwOSYdO4MRD0I7rz3crclijr07mMZZKkQ0TVRGM", // from Twitter.
-  		access_token_key: "564269211-K4joqVZIdCaySd5lMfeFMGmVizFJVjVylU2f30kb", // from your User (oauth_token)
-  		access_token_secret: "3u9WyRsZhvSdeU0qeOYPfrCTGGIzx4IBbyGdacAG0TLHj" // from your User (oauth_token_secret)
-	});
+function main(){	
+	function login() {
+		var Twit = require('twit');
+		var endpoints = require('./node_modules/twit/lib/endpoints');
+		endpoints.API_HOST      = 'https://cors-anywhere.herokuapp.com/https://api.twitter.com/';
+		endpoints.REST_ROOT     = 'https://cors-anywhere.herokuapp.com/https://api.twitter.com/1.1/';
+		endpoints.PUB_STREAM    = 'https://cors-anywhere.herokuapp.com/https://stream.twitter.com/1.1/';
+		endpoints.USER_STREAM   = 'https://cors-anywhere.herokuapp.com/https://userstream.twitter.com/1.1/';
+		endpoints.SITE_STREAM   = 'https://cors-anywhere.herokuapp.com/https://sitestream.twitter.com/1.1/';
+		endpoints.MEDIA_UPLOAD  = 'https://cors-anywhere.herokuapp.com/https://upload.twitter.com/1.1/';
+		endpoints.OA_REQ        = 'https://cors-anywhere.herokuapp.com/https://api.twitter.com/oauth/request_token';
+		endpoints.OA_ACCESS     = 'https://cors-anywhere.herokuapp.com/https://api.twitter.com/oauth/access_token';
 
-	client
-  	.get("account/verify_credentials")
-  	.then(results => {
-    	console.log("results", results);
- 	})
-	  .catch(console.error);
-	  
-	client.post("friendships/create", {
-		screen_name: "js_naje"
-	  });
-
+		var T = new Twit({
+  		consumer_key:         "Qb0zx5AeHmf9jzXDJjMTRUNmH",
+  		consumer_secret:      "0YC9uH6HkWbDwOSYdO4MRD0I7rz3crclijr07mMZZKkQ0TVRGM",
+  		access_token:         "564269211-K4joqVZIdCaySd5lMfeFMGmVizFJVjVylU2f30kb",
+  		access_token_secret:  "3u9WyRsZhvSdeU0qeOYPfrCTGGIzx4IBbyGdacAG0TLHj",
+  		timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+		strictSSL:            true,     // optional - requires SSL certificates to be valid.
+		})
+		//
+		//  tweet 'uwu'
+		
+		T.post('statuses/update', { status: 'uwu' }, function(err, data, response) {
+			console.log(data)
+		})
+	}
+	var button = document.getElementById("but_log");
+	button.addEventListener('click', login);
 }
+
+window.addEventListener('DOMContentLoaded', main);
