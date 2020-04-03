ab46823982e1dc66d4168cba1f2f3db88c5959d6
julien dewerpe <julien.dewerpe@orange.fr>
2020-04-03 12:33:05 +0200

    code refonted, impossible to select a cahmp again -> button invisible

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-    localStorage.setItem("count_p1", 0);
-    localStorage.setItem("count_p2", 0);
-    localStorage.setItem("champ_selected", []);
-});
+var state = {
+    count_p1: 0,
+    count_p2: 0,
+    champSelected: []
+};
+
 //todo : 
 /*
 
@@ -60,7 +61,11 @@ function addChampion(champion) {
     selectButton.appendChild(document.createTextNode("Select"));
     selectButton.className = "btn mt-3 btn-success float-right";
     selectButton.setAttribute("data-id", champion.id);
+    selectButton.setAttribute("id", "li" + champion.id);
     selectButton.setAttribute("data-champion", champion);
+    if (state.champSelected.find(e => e == champion.id) != null){
+        selectButton.style.display = "none";
+    }
 
     champElmt.className = "list-group-item";
     var img = champElmt.appendChild(image);
@@ -78,30 +83,61 @@ function addChampion(champion) {
 function selectChamp(champion) {
 
     var idPlayer = document.querySelector("#player-active").getAttribute("data-id");
-    var tmp = "count_p" + idPlayer;
-    console.log(tmp);
-    if (localStorage.getItem(tmp) < 3) {
-        var list = document.querySelector('#list-p' + idPlayer);
-        var champElmt = document.createElement("card");
-        champElmt.setAttribute("id", "card" + "_" + idPlayer + "_" + localStorage.getItem(tmp) + 1)
-        var image = document.createElement("img");
-        image.src = champion.image.url;
-        image.width = "100";
-        var img = champElmt.appendChild(image);
-        img.setAttribute("data-id", localStorage.getItem(tmp) + 1)
-        list.appendChild(champElmt);
+    if (idPlayer == 1){
+        var count_p = state.count_p1;
+    }
+    else {
+        var count_p = state.count_p2;
+    }
 
-        img.addEventListener('click', function (event) {
-            removeChamp(idPlayer, event.target.getAttribute("data-id"));
-        });
-
-        changePlayer();
-
-        localStorage.setItem(tmp, parseInt(localStorage.getItem(tmp)) + 1);
-        if (localStorage.getItem("count_p1") == 3 && localStorage.getItem("count_p2") == 3) {
-            teamsComplete();
+    if (parseInt(count_p) < 3) {
+        if (champIsSelected(champion)){
+            addChampInPlayerList(count_p, champion, idPlayer)
         }
     }
+}
+
+function addChampInPlayerList(count_p, champion, idPlayer) {
+    var list = document.querySelector('#list-p' + idPlayer);
+    var champElmt = document.createElement("card");
+    champElmt.setAttribute("id", "card" + "_" + idPlayer + "_" + (parseInt(count_p)+1));
+    champElmt.setAttribute("data-champ", champion.id);
+    var image = document.createElement("img");
+    image.src = champion.image.url;
+    image.width = "100";
+    image.height = "150";
+    var img = champElmt.appendChild(image);
+    img.setAttribute("data-id", (parseInt(count_p) + 1));
+    list.appendChild(champElmt);
+
+    img.addEventListener('click', function (event) {
+        removeChamp(idPlayer, event.target.getAttribute("data-id"));
+    });
+
+    changePlayer();
+
+    if (idPlayer == 1){
+        state.count_p1 = (parseInt(count_p)+1);
+    }
+    else {
+        state.count_p2 = (parseInt(count_p)+1)
+    }
+
+    if (state.count_p1 == 3 && state.count_p2 == 3) {
+        teamsComplete();
+    }
+}
+
+function champIsSelected(champion){
+    var champSelected = state.champSelected;
+    if (champSelected.find(e => e == champion.id) == null){
+        champSelected.push(champion.id);
+        var buttonToDisable = document.querySelector("#li"+champion.id);
+        buttonToDisable.style.display = "none";
+        return true;
+    }
+    return false
+    
 }
 
 
@@ -114,27 +150,40 @@ function teamsComplete(){
 
    
     startButton.className = "btn mt-3 btn-danger text_center";
-    //startButton.addEventListener('click', function () {
-    // faire le calcul
-    //});
+    startButton.addEventListener('click', function () {
+        //faire le calcul
+    });
 
 }
 
 
 function removeChamp(playerId, imageId) {
     var cardToDel = document.getElementById("card" + "_" + playerId + "_" + imageId);
+    var championId = cardToDel.getAttribute("data-champ");
+    var tmp = state.champSelected.indexOf(championId);
+    state.champSelected.splice(tmp,1);
+
+    var buttonToEnable = document.querySelector("#li"+championId);
+    buttonToEnable.style.display = "inline";
+
     cardToDel.remove();
-    var tmp = "count_p" + playerId;
-    localStorage.setItem(tmp, parseInt(localStorage.getItem(tmp)) - 1);
+
+    if (playerId == 1){
+        state.count_p1 = (parseInt(state.count_p1)-1);
+    }
+    else {
+        state.count_p2 = (parseInt(state.count_p2)-1)
+    }
+
     var startButton = document.querySelector("#fightButton");
     startButton.style.display = "none";
 
     var annou = document.getElementById("player-active");
-    if (localStorage.getItem("count_p1") < 3 && localStorage.getItem("count_p2") == 3) {
+    if (state.count_p1 < 3 && state.count_p2 == 3) {
         annou.innerHTML = "Player 1 select a champion";
         annou.setAttribute("data-id", 1);
     }
-    else if (localStorage.getItem("count_p2") < 3 && localStorage.getItem("count_p1") == 3) {
+    else if (state.count_p2 < 3 && state.count_p1 == 3) {
         annou.innerHTML = "Player 2 select a champion";
         annou.setAttribute("data-id", 2);
     }
@@ -143,11 +192,11 @@ function removeChamp(playerId, imageId) {
 
 function changePlayer() {
     var annou = document.getElementById("player-active");
-    if (annou.getAttribute("data-id") == 1 && localStorage.getItem("count_p2") < 3) {
+    if (annou.getAttribute("data-id") == 1 && state.count_p2 < 3) {
         annou.innerHTML = "Player 2 select a champion";
         annou.setAttribute("data-id", 2);
     }
-    else if (localStorage.getItem("count_p1") < 3) {
+    else if (state.count_p1 < 3) {
         annou.innerHTML = "Player 1 select a champion";
         annou.setAttribute("data-id", 1);
     }
